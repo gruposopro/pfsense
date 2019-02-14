a3c72e623eb8191aff75e8dbd51610ec5a8aed56
arielvitoria155 <42069904+arielvitoria155@users.noreply.github.com>
2019-02-14 13:03:00 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 !#/bin/sh
 
-pkg add http://pkg.freebsd.org/FreeBSD:11:amd64/latest/All/axel-2.4_2.txz
+pkg add http://pkg.freebsd.org/FreeBSD:11:amd64/latest/All/wget-1.20_2.txz
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-squid
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-squidguard
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-cron
@@ -10,7 +10,7 @@ env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-openvpn-client-export
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-Lightsquid 
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-Service_Watchdog
 
-axel https://raw.githubusercontent.com/arielvitoria155/pfsense/master/sgerror.php
+wget https://raw.githubusercontent.com/arielvitoria155/pfsense/master/sgerror.php
 
 cp sgerror.php /usr/local/www
 
