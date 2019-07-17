72257e5f9afbcbc9d44b17b3a8bdc87fb97adb67
arielvitoria155 <42069904+arielvitoria155@users.noreply.github.com>
2019-07-17 10:49:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ pkg add http://pkg.freebsd.org/FreeBSD:11:amd64/latest/All/wget-1.20.3.txz
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-squid
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-squidguard
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-cron
-env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-zabbix-agent34 
-env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-zabbix-proxy34
+env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-zabbix-agent4 
+env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-zabbix-proxy4
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-openvpn-client-export
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-Lightsquid 
 env ASSUME_ALWAYS_YES=YES pkg install pfsense-pkg-Service_Watchdog
